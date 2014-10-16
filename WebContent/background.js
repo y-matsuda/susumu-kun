cec32bdf379c6d664e2e3900f25391ea2ec1b545
y-matsuda <yuri.matsuda@change-vision.com>
2014-10-16 19:58:46 +0900

    refs #6 owner、repo、issues_numberを現在開いているページから取得したい
    * 正規表現をグローバル変数においた（書き方もちょっと変えた）
    * url、owner、repo、issues numberを取得できる関数を作成

--- WebContent/background.js
+++ WebContent/background.js
@@ -1,7 +1,11 @@
+var githubUrl = "";
+var issueUrlRegularExpression = new RegExp("https:\/\/github.com\/(.+)\/(.+)\/(issues|pulls)\/(.+)");
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	var url = tab.url;
-	if (url.match(new RegExp("https:\/\/github.com\/.+\/.+\/issues\/.+"))
-			|| url.match(new RegExp("https:\/\/github.com\/.+\/.+\/pulls\/.+"))) {
+	console.log("tab.url:" + url);
+	if (url.match(issueUrlRegularExpression)) {
+		this.url = url;
 		chrome.pageAction.setIcon({
 			'tabId' : tabId,
 			'path' : "icon.png"
@@ -9,3 +13,19 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		chrome.pageAction.show(tabId);
 	}
 });
+
+function getGithubUrl() {
+	return this.url;
+}
+
+function getGithubOwner() {
+	return this.url.match(issueUrlRegularExpression)[1];
+}
+
+function getGithubRepo() {
+	return this.url.match(issueUrlRegularExpression)[2];
+}
+
+function getGithubIssuesNumber() {
+	return this.url.match(issueUrlRegularExpression)[4];
+}
