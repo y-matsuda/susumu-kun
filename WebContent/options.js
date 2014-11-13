1cc353ddcfff45f8f3706c559c26086af65008f1
y-matsuda <yuri.matsuda@change-vision.com>
2014-11-13 18:58:06 +0900

    refs #13 Trelloのカードを探せるようにする
    * 設定画面の拡張
     * Trello のアプリケーションキーとアプリケーションアクセスキー、ボードIDを保存できるようにする

--- WebContent/options.js
+++ WebContent/options.js
@@ -2,10 +2,31 @@ function init() {
 	var githubAccessToken = localStorage.getItem("github_access_token");
 	console.log("githubAccessToken:" + githubAccessToken);
 	$("#github_access_token").val(githubAccessToken ? githubAccessToken : "");
+
+	var trelloApplicationKey = localStorage.getItem("trello_application_key");
+	console.log("trelloApplicationKey:" + trelloApplicationKey);
+	$("#trello_application_key").val(
+			trelloApplicationKey ? trelloApplicationKey : "");
+
+	var trelloApplicationAccessToken = localStorage
+			.getItem("trello_application_access_token");
+	console.log("trelloApplicationAccessToken:" + trelloApplicationAccessToken);
+	$("#trello_application_access_token").val(
+			trelloApplicationAccessToken ? trelloApplicationAccessToken : "");
+
+	var trelloBoardId = localStorage.getItem("trello_board_id");
+	console.log("trelloBoardId:" + trelloBoardId);
+	$("#trello_board_id").val(trelloBoardId ? trelloBoardId : "");
 }
 
 function save() {
-	localStorage.setItem("github_access_token", $("#github_access_token").val());
+	localStorage
+			.setItem("github_access_token", $("#github_access_token").val());
+	localStorage.setItem("trello_application_key", $("#trello_application_key")
+			.val());
+	localStorage.setItem("trello_application_access_token", $(
+			"#trello_application_access_token").val());
+	localStorage.setItem("trello_board_id", $("#trello_board_id").val());
 }
 
 init();
